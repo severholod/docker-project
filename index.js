0a28b3825ec160c3c9f94ad46b088c268b18d899
Ilya Severov <ilya.severov@mechanica.agency>
2022-03-24 20:43:10 +0400

    mongo compose

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port = 3000
 
+const logFile = path.resolve(__dirname, './express.log')
+
+function writeToLog(message) {
+  fs.writeFileSync(logFile, message.toString())
+}
+
+MongoClient.connect('mongodb://root:root@localhost:27017/testify', (err, database) => {
+  if (err) {
+    return writeToLog(err)
+  } else {
+    return writeToLog('success')
+  }
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!23!!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
