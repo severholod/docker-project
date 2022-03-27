e784a277d4093db9c024d00ee96dfe7916af2959
Ilya Severov <ilya.severov@mechanica.agency>
2022-03-27 14:34:21 +0400

    added collection

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
 const app = express()
-const port = 3000
+const PORT = 3000
+const URL = 'mongodb://mongo:27017'
 
-const logFile = path.resolve(__dirname, './express.log')
-
-function writeToLog(message) {
-  fs.writeFileSync(logFile, message.toString())
+const client = new MongoClient(URL)
+const run = async () => {
+  try {
+    await client.connect()
+    const db = client.db('root')
+    const collection = db.collection('users')
+    const user = {firstName: 'Tom', lastName: 'House', age: '42', position: 'manager'}
+    const res = await collection.insertOne(user)
+    return res
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await client.close()
+  }
 }
 
-MongoClient.connect('mongodb://root:root@localhost:27017/testify', (err, database) => {
-  if (err) {
-    return writeToLog(err)
-  } else {
-    return writeToLog('success')
-  }
-})
+app.get('/', async (req, res) => {
+  const response = await run()
 
-app.get('/', (req, res) => {
+  console.log(response)
   res.send('Hello World!23!!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
 
